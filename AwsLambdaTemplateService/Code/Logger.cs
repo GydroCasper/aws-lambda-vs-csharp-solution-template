25aeba9207b93a39df3510b2a6dc361371fa15fd
Nick Filatov <nfilatov@foundationmedicine.com>
2019-03-04 10:10:46 -0500

    Changed custom exception name

--- AwsLambdaTemplateService/Code/Logger.cs
+++ AwsLambdaTemplateService/Code/Logger.cs
@@ -9,11 +9,11 @@ namespace $safeprojectname$.Code
         public string LogException(Exception ex)
         {
             var errorId = Guid.NewGuid();
-            var body = ex is FmiException exception ? $"Body : {exception.Body} \r\n" : string.Empty;
+            var body = ex is CustomLambdaException exception ? $"Body : {exception.Body} \r\n" : string.Empty;
             var date = $"{DateTime.Now: yyyy MMMM dd HH:mm:ss.fff tt zz}";
             Console.WriteLine($"Message: {ex.Message} \r\n Date: {date} \r\n Error ID: {errorId} \r\n {body} StackTrace: {ex.StackTrace}");
 
-            var message = ex is FmiException ? ex.Message : Const.Messages.InternalError;
+            var message = ex is CustomLambdaException ? ex.Message : Const.Messages.InternalError;
             return $"Message: {message};  Date: {date};  Error ID: {errorId}";
         }
     }
